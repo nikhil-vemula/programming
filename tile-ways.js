e8bdcb0eb67feee2572edbea09baf6e09abce630
Nikhil Vemula <vsrinivasanikhil@gmail.com>
2022-04-15 12:21:41 +0530

    Add tile ways problem

---
+++ tile-ways.js
@@ -0,0 +1,35 @@
+/*
+Find the number of ways the 2x1 tile can be placed on 2xn space
+*/
+
+function f(n) {
+  if (n == 1)
+    return 1
+  
+  if (n == 2)
+    return 2
+  
+  return f(n-1) + f(n-2);
+}
+
+function f1(n) {
+  if (n == 1)
+    return 1;
+
+  if (n == 2)
+    return 2;
+
+  var t1 = 1, t2 = 2;
+  for (var i=3; i <= n; i++) {
+    var temp = t1 + t2;
+    t1 = t2;
+    t2 = temp;
+  }
+  return t2;
+}
+
+console.log(f1(1)) // 1
+console.log(f1(2)) // 2
+console.log(f1(3)) // 3
+console.log(f1(4)) // 5
+console.log(f1(5)) // 8
